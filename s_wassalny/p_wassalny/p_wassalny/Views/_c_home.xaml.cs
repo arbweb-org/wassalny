cfe7e020a7ff270900d3a1479c1e864556c22891
yassin-soliman <yassin_soliman@outlook.com>
2020-08-27 23:42:22 +0200

    Initial commit

---
+++ s_wassalny/p_wassalny/p_wassalny/Views/_c_home.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.ComponentModel;
+
+namespace p_wassalny
+{
+    public partial class _c_home : ContentPage
+    {
+        public _c_home()
+        {
+            InitializeComponent();
+        }
+    }
+}
